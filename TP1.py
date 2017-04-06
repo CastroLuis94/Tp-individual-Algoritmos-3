39c6e3ed6274195ed63d02d72a4796a347fc858a
Luiscastro1994 <luisitocastro94@hotmail.com>
2017-04-06 14:03:52 -0300

    Update TP1.py

--- TP1.py
+++ TP1.py
@@ -1,59 +1,99 @@
 import timeit
 import random
-from auxiliares import *
+import copy
 
-
-#print ('Ingrese los numeros del arreglo.')
+#print('Ingrese los numeros del arreglo.')
 #entrada = input()
 
+def transformar_input(entrada):
+    res = []
+    for num in entrada.split():
+        res.append(int(num))
+    return res
 
 #entrada = transformar_input(entrada)
 
-
-#O(N)
-def elige_resultado(posibles_res):
-    suma_maxima = 0
-    res = []
-    for elem in posibles_res:
-        if(suma_maxima < len(elem[0])+len(elem[1]) ):
-            suma_maxima = len(elem[0])+len(elem[1])
-            res = elem[0]+elem[1]
-    return (res,suma_maxima)
-
-
-def ejercicio(entrada): 
-    lista_de_rojos = rojos(entrada)
-    #O(N*(2**N))
-    res = []
-    cantidad_de_rojos = len(lista_de_rojos)
-    #O(1) 
-    for indice in range(0,cantidad_de_rojos): #O(N*(2**N))
-        res.append((lista_de_rojos[indice],mas_largo(azules(simplificar(entrada,lista_de_rojos[indice])))))
+class Pintar():
+    def __init__(self):
+        self.indice = 0
+        self.ulti_rojo = None
+        self.ulti_azul = None
+        self.pintados = 0
         
-    return (res)
+    def semi_copiador(self):
+        res = Pintar()
+        res.indice = copy.deepcopy(self.indice)
+        res.ulti_azul = copy.deepcopy(self.ulti_azul)
+        res.ulti_rojo = copy.deepcopy(self.ulti_rojo)
+        res.pintados = copy.deepcopy(self.pintados)
+        return res
 
-def ejercicio_uno(entrada):
-    conj_rojos = rojos(entrada)
-    conj_azules = azules(entrada)
+    def pintar(self,a_pintar):
+        if len(a_pintar) == 0:
+            return self.pintados
+        else:
+            copia1 = self.semi_copiador()
+            copia2 = self.semi_copiador()
+            copia3 = self.semi_copiador()
+            return max(copia1.pintar_rojo(a_pintar), copia2.pintar_azul(a_pintar), copia3.no_pintar(a_pintar))
+    
+    def pintar_rojo(self,a_pintar):
+        if len(a_pintar) <= self.indice:
+            return self.pintados
+        else:
+            if (self.ulti_rojo) == None or a_pintar[self.indice] > self.ulti_rojo:
+                self.ulti_rojo = a_pintar[self.indice]
+                self.indice += 1
+                self.pintados += 1
+                return self.pintar(a_pintar)
+            else:
+                self.indice += 1
+                return self.pintar(a_pintar)
+
+    def pintar_azul(self,a_pintar):
+        if len(a_pintar) <= self.indice:
+            return self.pintados
+        else:
+            if self.ulti_azul is None or a_pintar[self.indice] < self.ulti_azul:
+                self.ulti_azul = a_pintar[self.indice]
+                self.indice += 1
+                self.pintados += 1
+                return self.pintar(a_pintar)
+            else:
+                self.indice += 1
+                return self.pintar(a_pintar)
+        
+    def no_pintar(self,a_pintar):
+        if len(a_pintar) <= self.indice:
+            return self.pintados
+        else:
+            self.indice += 1
+            return self.pintar(a_pintar)
+
+def generador_entradas_random(cantidad_de_muestras,longitud_de_muestra):
+    muestra = []
     res = []
-    for rojo in conj_rojos:
-        for azul in conj_azules:
-            if rojo & azul == set() :
-                res.append((rojo,azul,len(rojo)+len(azul)))
-    return len(entrada) - (buscar_mas_largo(res)[2])
+    for muestra_numero in range(0,cantidad_de_muestras):
+        for elem in range(0,longitud_de_muestra):
+            muestra.append(random.randint(1,100))
+        res.append(muestra)
+        muestra = []
+    return res
 
 
+start_time = timeit.default_timer()
 
-tamanio_muetras = range(0,26)
+print(timeit.default_timer() - start_time)
+
+tamanio_muetras = range(0,11)
 tiempos = [] 
-numero_de_muestras = 1
+numero_de_muestras = 100
 for tamanio_muestra in tamanio_muetras:   
     muestras = (generador_entradas_random(numero_de_muestras,tamanio_muestra))
     start_time = timeit.default_timer()
     for muestra in muestras:
-       ejercicio_uno(muestra)
+        pintador = Pintar()
+        pintador.pintar(muestra)
     tiempo = (tamanio_muestra,(timeit.default_timer() - start_time)/float(numero_de_muestras))
     tiempos.append(tiempo)
     print (tiempo)
-
-
