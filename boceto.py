cdf58676abaa500d80f4aeaa95889a5c65fe974b
Luiscastro1994 <luisitocastro94@hotmail.com>
2017-04-13 03:39:13 -0300

    Boceto a pasar en limpio

--- boceto.py
+++ boceto.py
@@ -1,44 +1,39 @@
 import timeit
+import copy
 
 
 class Pintador():
     def __init__(self):
-        self.indice = 0
         self.ulti_rojo = None
-        self.ulti_azul = None
-        self.pintados = 0
+        self.max_azul = None
 
     def semi_copiador(self,otro):
         res = Pintador()
-        res.indice = otro.indice
-        res.ulti_azul = otro.ulti_azul
+        res.max_azul = otro.max_azul
         res.ulti_rojo = otro.ulti_rojo
-        res.pintados = otro.pintados
         return res
     def puedo_pintar_rojo(self,numero):
         return (self.ulti_rojo == None) or (self.ulti_rojo < numero)
     
     def puedo_pintar_azul(self,numero):
-        return (self.ulti_azul == None) or (self.ulti_azul > numero)
+        return (self.max_azul == None) or (self.max_azul > numero)
     
     def pintar_azul(self,numero):
         copia1 = self.semi_copiador(self)
-        copia1.ulti_azul = numero
-        copia1.pintados += 1
+        copia1.max_azul = numero
         return copia1
     def pintar_rojo(self,numero):
         copia1 = self.semi_copiador(self)
         copia1.ulti_rojo = numero
-        copia1.pintados += 1
         return copia1
 
     def __str__(self):
-        return '(' + str(self.ulti_rojo) + ',' + str(self.ulti_azul) + ',' + str(self.pintados) + ')'
+        return '(' + str(self.ulti_rojo) + ',' + str(self.max_azul) + ')'
     
     def __repr__(self):
-        return '(' + str(self.ulti_rojo) + ',' + str(self.ulti_azul) + ',' + str(self.pintados) + ')'
+        return '(' + str(self.ulti_rojo) + ',' + str(self.max_azul) + ')'
     def __eq__(self,otro):
-        return self.ulti_azul == otro.ulti_azul and self.ulti_rojo == otro.ulti_rojo and self.pintados == otro.pintados
+        return self.max_azul == otro.max_azul and self.ulti_rojo == otro.ulti_rojo
 
 def levanta_archivo(nombre):
     archivo = open(nombre, "r") 
@@ -74,75 +69,90 @@ def potencial(arreglo,indice):
                 mayores += 1
         indice+=1
     return max(mayores,menores)
-#arreglos = [[3, 11, 0, 1, 3, 5, 2, 4, 1, 0, 9]]
-#arreglos = [[0,7,1,2,2,1,5,0]]
-arreglos = levanta_archivo("t16")
-print(len(arreglos))
+
+
+def esta_el_rojo(lista,rojo):
+    for elem in lista:
+        if elem.ulti_rojo == rojo:
+            return True
+    return False
+
+def dame_el_rojo(lista,rojo):
+    for elem in lista:
+        if elem.ulti_rojo == rojo:
+            return elem
+
+
+#arreglos = [[1,2,3]]
+#arreglos = [[3, 11, 0, 1, 3, 5, 2, 4, 1, 0, 9, 3]]
+arreglos = levanta_archivo("muestra_tamanio1000")
+#print(len(arreglos))
 tiempo = timeit.default_timer()
+numero_elemento = 0
 for arreglo in arreglos:
-   
+
+
+
+
+    
     i = 0
     res = []
     while(i < len(arreglo)+1):
         res.append([])
         i+=1
-    res[0] = [Pintador()]
+    res[0].append(Pintador())
+    max_pintados = 0
+    i=0
+    while i < len(arreglo):
+        k = i+1
+        while k >= 0:
+            for elem in res[k]:
+                if(elem.ulti_rojo == None or elem.ulti_rojo < arreglo[i]):
+                    nuevo_pintador = Pintador()
+                    nuevo_pintador.ulti_rojo = arreglo[i]
+                    nuevo_pintador.max_azul = elem.max_azul
+                    if not esta_el_rojo(res[k+1],nuevo_pintador.ulti_rojo):
+                        res[k+1].append(nuevo_pintador)
+                    else:
+                        rojo = dame_el_rojo(res[k+1],nuevo_pintador.ulti_rojo)
+                        if (rojo.max_azul != None) and (nuevo_pintador.max_azul == None or rojo.max_azul < nuevo_pintador.max_azul):
+                            rojo.max_azul = nuevo_pintador.max_azul
+                if(elem.max_azul == None or elem.max_azul > arreglo[i]):
+                    nuevo_pintador = Pintador()
+                    nuevo_pintador.ulti_rojo = elem.ulti_rojo
+                    nuevo_pintador.max_azul = arreglo[i]
+                    if not  esta_el_rojo(res[k+1],elem.ulti_rojo):
+                         res[k+1].append(nuevo_pintador)
+                    else:
+                        rojo = dame_el_rojo(res[k+1],nuevo_pintador.ulti_rojo)
+                        if (rojo.max_azul != None) and (nuevo_pintador.max_azul == None or rojo.max_azul < nuevo_pintador.max_azul):
+                            rojo.max_azul = nuevo_pintador.max_azul
+            k-=1
+        i+=1
+        print(i)
+
+
+
+
+
+
+
+
     
-    maxima_cant_pintados = 0
-    posicion = 0
-    i = maxima_cant_pintados
-    while posicion < len(arreglo):
-       
-        if  maxima_cant_pintados-  (len(arreglo)-posicion) > 0: 
-            cantidad_iteraciones = min(maxima_cant_pintados, maxima_cant_pintados -(len(arreglo)-posicion ))
-        else:
-            cantidad_iteraciones = maxima_cant_pintados
-        while(cantidad_iteraciones >= 0 ):
-            k = 0
-            pintados = []
-            tamanio = len(res[i])
-            while(k < tamanio):
-                if(res[i][k].puedo_pintar_rojo(arreglo[posicion])):
-                    pintados.append( (res[i][k]).pintar_rojo(arreglo[posicion]) )
-                if(res[i][k].puedo_pintar_azul(arreglo[posicion])):
-                    pintados.append( (res[i][k]).pintar_azul(arreglo[posicion]) )
-                k += 1
-            p = 0
-            while(p < len(pintados)):
-                x = 0
-                while x < len(res[i+1]):
-                    if res[i+1][x] == pintados[p]:
-                        break
-                    x+=1
-                if(x==len(res[i+1])):
-                    res[i+1].append(pintados[p])
-                p+=1
-            i-=1
-            cantidad_iteraciones -=1
-        posicion += 1
-        if(len(res[i+1]) > 0 ):
-            maxima_cant_pintados += 1
-            i = maxima_cant_pintados
-    #print(res)
-    """
-    if(len(pintados) > 0 or i == maxima_cant_pintados):
-                break
-    """
-
-    ultimo = len(res) -1
-    while(ultimo >= 0):  
-        if(len(res[ultimo]) != 0):
-            print ((res[ultimo][0]))
+    ultimo = len(res)-1
+    while(ultimo >= 0 ):
+        if len(res[ultimo]) != 0 :
+            print(len(arreglo) -ultimo)
             break
-        ultimo -=1
-
+        ultimo -= 1
+    
+    
+    
+    print(timeit.default_timer() - tiempo)
     cantidad = 0
     for elem in res:
         cantidad += len(elem)
     print(cantidad)
-print(timeit.default_timer() - tiempo)
-
-
 
    
             
